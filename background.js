8df24c4a4b42ab15404b0653f604d798f3a7c50d
gwoods22 <graemewoods202@gmail.com>
2018-07-30 15:20:04 -0400

    add support for JQuery

--- background.js
+++ background.js
@@ -10,4 +10,7 @@ chrome.omnibox.onInputChanged.addListener(
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
     chrome.tabs.executeScript(null, {code: text});
+    chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
+      chrome.tabs.executeScript(null, {code: text});
+    });
   });
